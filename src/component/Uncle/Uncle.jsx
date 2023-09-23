ad2fe95bc3d8696ca0fe5b763a0bc7d50b77cd76
najatul6 <mdnajatulislam.develop@gmail.com>
2023-09-23 07:40:07 +0600

    Edit uncle file and add one cousin name that need

--- src/component/Uncle/Uncle.jsx
+++ src/component/Uncle/Uncle.jsx
@@ -6,6 +6,7 @@ const Uncle = ({asset}) => {
             <h2>Uncle</h2>
             <section className="flex">
                 <Cousin asset={asset} name={'Rofiq'}></Cousin>
+                <Cousin name={'Sunny'}></Cousin>
                 <Cousin name={'Sana'}></Cousin>
             </section>
         </div>
