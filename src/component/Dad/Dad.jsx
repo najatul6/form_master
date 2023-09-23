ef81edc000658fc92caf1bdd4531d923ae3fd722
najatul6 <mdnajatulislam.develop@gmail.com>
2023-09-23 07:38:30 +0600

    Change some text and Spelling mistake completely solved

--- src/component/Dad/Dad.jsx
+++ src/component/Dad/Dad.jsx
@@ -5,7 +5,7 @@ import Sister from "../Sister/Sister";
 const Dad = ({asset}) => {
     return (
         <div>
-            <h2>Daddy </h2>
+            <h2>Dad</h2>
             <section className="flex">
                 <Myself asset={asset}></Myself>
                 <Brother></Brother>
